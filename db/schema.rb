2e4490d283f97bfbcc0888c27bc12a7c1c6587c3
minsoo cho <khg04170@gmail.com>
2022-03-28 13:48:22 +0100

    cloudinary setup done,

--- db/schema.rb
+++ db/schema.rb
@@ -76,7 +76,7 @@ ActiveRecord::Schema.define(version: 2022_03_28_101827) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
